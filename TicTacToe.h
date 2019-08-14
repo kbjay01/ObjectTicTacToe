5b6ef41d70ce272ac386175e1adf99e66b0d4a1f
Jakub <39642297+kbjay01@users.noreply.github.com>
2019-08-14 20:11:08 +0200

    Update TicTacToe.h

--- TicTacToe.h
+++ TicTacToe.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <iostream>
 #include <ctime>
+#include <array>
 #define String std::string
 #define uchar unsigned char
 #define uint unsigned int
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-enum PLAYER_TYPE{ PL_CIRCLE = 'O', PL_CROSS = 'X', PL_NOTSIGNED };
+enum PLAYER_TYPE { PL_CIRCLE = 'O', PL_CROSS = 'X', PL_NOTSIGNED };
 enum GAME_STATE { GS_NOTSTARTED, GS_STARTED, GS_MOVE, GS_WON, GS_DRAW, GS_ENDED };
 enum FIELD {
 	FLD_EMPTY,
 	FLD_CIRCLE = PL_CIRCLE,
 	FLD_CROSS = PL_CROSS
 };
- 
 
-class Players{
+
+class Players {
 private:
 	String player1_name, player2_name; // names of player one, and player two
 	PLAYER_TYPE player1_char, player2_char; // player but with assigned chars (circle, cross)
@@ -28,25 +31,27 @@ public:
 	}
 
 	String getPlayerName(__int8 nameID); // returns name of player one, or player two
-	void askAndSaveGivenNames(); // asks users for their names and saves them to variables player1_name and player2_name
-	void assignRandomSignToPlayerName(); // assigns chars to variables player1_char and player2_char
-	void drawWhosFirst(); // draw which player is going to start first
-	void nextPlayer(); // changes player after move had without win or draw occured
+	void AskAndSetGivenNames(); // asks users for their names and saves them to variables player1_name and player2_name
+	void SetRandomSignToPlayerName(); // assigns chars to variables player1_char and player2_char
+	void DrawWhosFirst(); // draw which player is going to start first
+	void NextPlayer(); // changes player after move had without win or draw occured
 };
 
-class Map{
+class Map {
 private:
-	FIELD field_map[3][3];
+	FIELD FIELD_MAP[3][3];
 	uchar LINES[][3][2];
-	
+
 public:
-	void drawHeaderAndMap(); // Draws map into console
-	bool checkIfMoveIsLegalAndMove(uchar); // checks if outsourced move is possible, if so makes the move
-	bool checkIfSomeoneWon(); // checks if someone won
-	bool checkIfDrawOccured(); // checks if draw occured
-	void createFieldMap(); // fils array named "field_map" with FIELD values
-	void createLinesArray(); // fils array named "correct_lines" with correct/winnable lines
-	
+	void DrawHeaderAndMap(); // Draws map into console
+	bool CheckIfMoveIsLegalAndMove(uchar); // checks if outsourced move is possible, if so makes the move
+	bool CheckIfSomeoneWon(); // checks if someone won
+	bool CheckIfDrawOccured(); // checks if draw occured
+	void CreateFieldMap(); // fils array named "field_map" with FIELD values
+	void CreateLinesArray(); // fils array named "correct_lines" with correct/winnable lines
+
+	Map();
+	~Map();
 };
 
 class Game {
