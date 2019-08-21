4c61813745e3421f4226395f64811857d325348f
Jakub <39642297+kbjay01@users.noreply.github.com>
2019-08-21 19:29:14 +0200

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,74 +1,84 @@
 #include "TicTacToe.h"
 
 //**GAME**
-void Game::StartGame() {
+void Game::StartNewGame() {
 	GameState = GS_STARTED;
+}
+
+void Game::DrawHeader() {
+	system("cls");
+	std::cout << "     Objective TicTacToe     " << std::endl;
+	std::cout << "-----------------------------" << std::endl;
+	std::cout << "" << std::endl;
+}
+
+void Game::PauseUntilKeyIsPressed() {
+	_getch();
 }
 
 // *********************
 // **PLAYERS**
 // *********************
-String Players::getPlayerName(__int8 nameID){
+std::string Players::GetPlayerName(__int8 nameID){
 	return (nameID == 1 ? player1_name : player2_name);
+}
+
+PLAYER_TYPE Players::GetPlayerChar(__int8 nameID) {
+	return (nameID == 1 ? Player1Char : Player2Char);
 }
 
 void Players::AskAndSetGivenNames() {
 
 	Game::GameState = GS_STARTED;
-	std::cout << "Enter player1 (circle) name: ";
+	std::cout << "Enter player1 name: ";
 	std::cin >> player1_name;
-	std::cout << "Enter player2 (cross) name: ";
+	std::cout << "Enter player2 name: ";
 	std::cin >> player2_name;
+	std::cout << std::endl;
 }
 
 void Players::SetRandomSignToPlayerName() {
 
 	srand(static_cast<uint>(time(NULL)));
-	player1_char = rand() % 2 == 0 ? PL_CIRCLE : PL_CROSS;
-	player1_char == PL_CIRCLE ? player2_char = PL_CROSS : PL_CIRCLE;
+	Player1Char = rand() % 2 == 0 ? PL_CIRCLE : PL_CROSS;
+	Player1Char == PL_CIRCLE ? Player2Char = PL_CROSS : PL_CIRCLE;
 }
  
 void Players::DrawWhosFirst() {
-	current_player = rand() % 2 == 0 ? PL_CIRCLE : PL_CROSS;
+	CurrentPlayer = rand() % 2 == 0 ? PL_CIRCLE : PL_CROSS;
 }
 
 void Players::NextPlayer() {
-	current_player = (current_player == PL_CIRCLE ? PL_CROSS : PL_CIRCLE);
+	CurrentPlayer = (CurrentPlayer == PL_CIRCLE ? PL_CROSS : PL_CIRCLE);
 }
+
 
 // **********************
 // **MAP**
 // *********************
 
 // Constructor
-Map::Map() : LINES{
+Map::Map() : Lines{
 
- { { 0,0 }, { 0,1 }, { 0,2 } }, // top horizontal
- { { 1,0 }, { 1,1 }, { 1,2 } },// mid vertcal
- { { 2,0 }, { 2,1 }, { 2,2 } },// bottom horizontal
- { { 0,0 }, { 1,0 }, { 2,0 } }, // left vertcal
- { { 0,1 }, { 1,1 }, { 2,1 } }, // mid vertcal
- { { 0,2 }, { 1,2 }, { 2,2 } }, // right vertcal
- { { 0,0 }, { 1,1 }, { 2,2 } }, // right backslash
- { { 2,0 }, { 1,1 }, { 0,2 } } } // right slash 
+	{ { 0,0 }, { 0,1 }, { 0,2 } }, // top horizontal
+	{ { 1,0 }, { 1,1 }, { 1,2 } },// mid vertcal
+	{ { 2,0 }, { 2,1 }, { 2,2 } },// bottom horizontal
+	{ { 0,0 }, { 1,0 }, { 2,0 } }, // left vertcal
+	{ { 0,1 }, { 1,1 }, { 2,1 } }, // mid vertcal
+	{ { 0,2 }, { 1,2 }, { 2,2 } }, // right vertcal
+	{ { 0,0 }, { 1,1 }, { 2,2 } }, // right backslash
+	{ { 2,0 }, { 1,1 }, { 0,2 } } } // right slash 
 
 {
 	for (char i = 0; i < 3; i++) {
 		for (char j = 0; j < 3; j++) {
 			// Filling whole array with FLD_EMPTY values
-			FIELD_MAP[i][j] = FLD_EMPTY;
+			FieldMap[i][j] = FLD_EMPTY;
 		}
-	}	
+	}
 }
 
-void Map::DrawHeaderAndMap() {
-
-	// *HEADER*
-	std::cout << "     Objective TicTacToe     " << std::endl;
-	std::cout << "-----------------------------" << std::endl;
-	std::cout << "" << std::endl;
-
-	// *BODY*
+void Map::DrawMap() {
 
 	for (uchar i = 0; i < 3; ++i) {
 		// Left frame
@@ -76,12 +86,12 @@ void Map::DrawHeaderAndMap() {
 
 		// Line
 		for (uchar j = 0; j < 3; ++j) {
-			if (FIELD_MAP[i][j] == FLD_EMPTY) {
+			if (FieldMap[i][j] == FLD_EMPTY) {
 				// field ID
 				std::cout << i * 3 + j + 1;
 			}
 			else {
-				std::cout << static_cast<char>(FIELD_MAP[i][j]);
+				std::cout << static_cast<char>(FieldMap[i][j]);
 			}
 		}
 		// Right frame
@@ -102,8 +112,8 @@ bool Map::CheckIfMoveIsLegalAndMove(uchar fieldID) {
 	uint y = (fieldID - 1) % 3;
 
 	// Check if field can be taken
-	if (FIELD_MAP[x][y] == FLD_EMPTY) {
-		FIELD_MAP[x][y] = static_cast<FIELD>(Players::current_player);
+	if (FieldMap[x][y] == FLD_EMPTY) {
+		FieldMap[x][y] = static_cast<FIELD>(Players::CurrentPlayer);
 		return true;
 	}
 	else return false; // Impossible
@@ -112,32 +122,32 @@ bool Map::CheckIfMoveIsLegalAndMove(uchar fieldID) {
 bool Map::CheckIfSomeoneWon() {
 
 	FIELD Field, CorrectField;
-	uchar CorrectFieldCounter;
+	uchar correct_field_counter;
 	for (uchar i = 0; i < 8; ++i)
 	{
 		// i iterates every possible line (there's 8 of them)
 		// setting default values to variables
 		Field = CorrectField = FLD_EMPTY;
-		CorrectFieldCounter = 0;
+		correct_field_counter = 0;
 		for (uchar j = 0; j < 3; ++j)
 		{
 			// j iterates every 3 fields in every line
 			// We're getting field
-			Field = FIELD_MAP[LINES[i][j][0]][LINES[i][j][1]];
+			Field = FieldMap[Lines[i][j][0]][Lines[i][j][1]];
 			// if checked field is diffrent than correct field...
 			if (Field != CorrectField)
 			{
 				// set correct field to current field
 				CorrectField = Field;
-				CorrectFieldCounter = 1;
+				correct_field_counter = 1;
 			}
 			else
 				// if both fields are equal
 				// increment CorrectFieldCounter
-				++CorrectFieldCounter;
+				++correct_field_counter;
 		}
 		// check if whole line is winnable
-		if (CorrectFieldCounter == 3 && CorrectField != FLD_EMPTY)
+		if (correct_field_counter == 3 && CorrectField != FLD_EMPTY)
 		{
 			// if yes, set GameState to Won
 			Game::GameState = GS_WON;
@@ -145,6 +155,7 @@ bool Map::CheckIfSomeoneWon() {
 			return true;
 		}
 	}
+	return false;
 }
 
 bool Map::CheckIfDrawOccured() {
@@ -152,7 +163,7 @@ bool Map::CheckIfDrawOccured() {
 	uchar amount_Of_Filled_Fields = 0;
 	for (uchar i = 0; i <= 3; i++) {
 		for (uchar j = 0; j < 3; j++) {
-			if (FIELD_MAP[i][j] != FLD_EMPTY) {
+			if (FieldMap[i][j] != FLD_EMPTY) {
 				++amount_Of_Filled_Fields;
 			}
 		}
